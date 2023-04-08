b6b6012df654b510a434891ca4c872a47c30e0f1
Amy Miles <missamylmiles@gmail.com>
2023-04-08 16:22:14 -0500

    added attributes and methods

--- src/model/Boat.java
+++ src/model/Boat.java
@@ -19,6 +19,34 @@ package model;
 * my program.         
 ***************************************************************/
 
-public class Node {
-
+public class Boat {
+	
+	private String name;
+	private int capacity;
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the capacity
+	 */
+	public int getCapacity() {
+		return capacity;
+	}
+	/**
+	 * @param capacity the capacity to set
+	 */
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+	
+	
 }
---
+++ src/model/Person.java
@@ -0,0 +1,129 @@
+package model;
+
+/**************************************************************
+* Name        : 
+* Author      : Amy Miles
+* Created     : Apr 8, 2023
+* Course      : CIS 152 Data Structures
+* Version     : 20H2
+* OS          : Windows 10
+* Copyright   : This is my own original work based on
+*               specifications issued by our instructor
+* Description : This program overall description here
+*               Input:  list and describe
+*               Output: list and describe
+* Big O		  : 
+* Academic Honesty: I attest that this is my original work.
+* I have not used unauthorized source code, either modified or 
+* unmodified. I have not given other fellow student(s) access to
+* my program.         
+***************************************************************/
+//Node Class
+public class Person {
+
+	//members
+	private String name;
+	private String age;
+
+	private int years = 0;//default
+	private int months = 0;//default
+	private boolean considerations = false;//default
+	private String considerText;
+	
+	public Person(String name, int years, int months) {
+		this.name = name;
+		this.years = years;
+		this.months = months;	
+	}	
+	/**
+	 * @return the age
+	 */
+	public String getAge() {
+		if (getMonths() == 0){
+			return getYears() + " years";
+		}
+		else
+		return getMonths() + " months";
+	}
+	/**
+	 * @param age the age to set
+	 */
+	public void setAge(String age) {
+		this.age = age;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the years
+	 */
+	public int getYears() {
+		return years;
+	}
+	/**
+	 * @param years the years to set
+	 */
+	public void setYears(int years) {
+		this.years = years;
+	}
+	/**
+	 * @return the months
+	 */
+	public int getMonths() {
+		return months;
+	}
+	/**
+	 * @param months the months to set
+	 */
+	public void setMonths(int months) {
+		this.months = months;
+	}
+
+	/**
+	 * @return the considerations
+	 */
+	public boolean isConsiderations() {
+		return considerations;
+	}
+	/**
+	 * @param considerations the considerations to set
+	 */
+	public void setConsiderations(boolean considerations) {
+		this.considerations = considerations;
+	}
+	/**
+	 * @return the considerText
+	 */
+	public String getConsiderText() {
+		return considerText;
+	}
+	/**
+	 * @param considerText the considerText to set
+	 */
+	public void setConsiderText(String considerText) {
+		this.considerText = considerText;
+	}
+	@Override
+	public String toString() {
+		if (isConsiderations()) {
+			return getName() + " " + getAge() + " " + " Special Considerations: " +
+					getConsiderText();
+		}else
+		return getName() + " " + getAge() ;
+	}
+	
+	
+	
+	
+	
+	
+}
